22506231b6527b44e559aae2f5763ace3f9d6c25
MossoIsai <ing.isaimosso@gmail.com>
2017-04-22 14:13:14 -0500

    Listener del tab

--- app/src/main/java/com/blueicon/tablayoutsimple/MainActivity.java
+++ app/src/main/java/com/blueicon/tablayoutsimple/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.*;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,77 +30,18 @@ public class MainActivity extends Fragment {
         tabLayout.addTab(tabLayout.newTab().setText("Tab 5"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                System.out.println("POSICION: "+tab.getPosition());
-                inflateFragmenSelectec(tab.getPosition());
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-        //CostumerFragment2 adapter = new CostumerFragment2(getActivity().getSupportFragmentManager());
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                // Here position is the selected page-position
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        return rootView;
+        CostumerFragment2 adapter = new CostumerFragment2(getChildFragmentManager());
+        viewPager.setAdapter(adapter);
+        return  rootView;
     }
-
-    public Fragment inflateFragmenSelectec(int position){
-        switch (position){
-            case 0:
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                Fragment1 fragment1 = new Fragment1();
-                fragmentTransaction.add(R.id.viewPager_menu, fragment1);
-                fragmentTransaction.replace(R.id.viewPager_menu, fragment1);
-                fragmentTransaction.commit();
-                return  new Fragment1();
-
-            case 1:
-                FragmentManager fragmentManager2 = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
-                Fragment2 fragment2 = new Fragment2();
-                fragmentTransaction2.add(R.id.viewPager_menu, fragment2);
-                fragmentTransaction2.replace(R.id.viewPager_menu, fragment2);
-                fragmentTransaction2.commit();
-                return new  Fragment2();
-
-            case 2: return  new Fragment3();
-            case 3: return  new Fragment4();
-            case 4: return  new Fragment5();
-        }
-        return  null;
-    }
-
 
     public class CostumerFragment2 extends FragmentStatePagerAdapter {
+
+        public void loadData() {
+
+        }
+
+            // your cod
 
         public CostumerFragment2(FragmentManager fm) {
             super(fm);
@@ -109,66 +49,42 @@ public class MainActivity extends Fragment {
         }
         @Override
         public Fragment getItem(int index) {
-            System.out.println("POSICION: " + index);
+            System.out.println("CONSOLE 1 POSICION A ELIMINAR: "+(index));
 
-            switch (index) {
-                case 0:
-
-                    return new Fragment1();
-
-                case 1:
-
-                    return new Fragment2();
-                case 2:
-
-                    return new Fragment3();
-                case 3:
-
-                    return new Fragment4();
-                case 4:
-
-                    return new Fragment5();
-
-
-            }
-            return null;
-
-
+            switch (index){
+               case 0:
+                   return  new Fragment1();
+               case 1:
+                   return  new Fragment2();
+               case 2:
+                   return  new Fragment3();
+               case 3:
+                   return  new Fragment4();
+               case 4:
+                   return  new Fragment5();
+           }
+           return null;
         }
-
         @Override
         public int getCount() {
             return 5;
         }
+        //Borrame todo lo que no esete visible
+        @Override
+        public int getItemPosition(Object object) {
+            return  POSITION_NONE;
+        }
 
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+            System.out.println("CONSOLE 2 POSITION DISTRUIDA: " +position);
+        }
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
 
-    }
+        }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d("CONSOLE", "Main :::::::::: onResume");
-
-
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        Log.d("CONSOLE", "Main :::::::::: onPause");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d("CONSOLE", "Main :::::::::: onStart");
-
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
     }
 
 }
--- app/src/main/java/com/blueicon/tablayoutsimple/Principal.java
+++ app/src/main/java/com/blueicon/tablayoutsimple/Principal.java
@@ -22,81 +22,47 @@ public class Principal extends AppCompatActivity {
     private AHBottomNavigation ahBottomNavigation;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("CONSOLE","PAPA onCreate");
+        Log.d("CONSOLE", "PAPA onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
 
         frameLayoutFragment = (FrameLayout) findViewById(R.id.myFragemnt);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
-        ahBottomNavigation = (AHBottomNavigation)findViewById(R.id.bottom_navigation);
+        ahBottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
-        final AHBottomNavigationItem item1 = new AHBottomNavigationItem("Menú",R.drawable.menu);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Favoritos",R.drawable.favoritos_item);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Tu orden",R.drawable.item_charola);
-        AHBottomNavigationItem item4 = new AHBottomNavigationItem("Perfil",R.drawable.item_perfil);
+        final AHBottomNavigationItem item1 = new AHBottomNavigationItem("Menú", R.drawable.menu);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Favoritos", R.drawable.favoritos_item);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Tu orden", R.drawable.item_charola);
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem("Perfil", R.drawable.item_perfil);
 
         ahBottomNavigation.addItem(item1);
         ahBottomNavigation.addItem(item2);
         ahBottomNavigation.addItem(item3);
         ahBottomNavigation.addItem(item4);
-
-        //ahBottomNavigation.setDefaultBackgroundColor(R.color.colorAccent);
-
-// Disable the translation inside the CoordinatorLayout
         ahBottomNavigation.setBehaviorTranslationEnabled(false);
-
-// Enable the translation of the FloatingActionButton
-       // ahBottomNavigation.manageFloatingActionButtonBehavior(floatingActionButton);
-
-// Change colors
         ahBottomNavigation.setAccentColor(R.color.colorAccent);
         ahBottomNavigation.setInactiveColor(R.color.colorPrimary);
 
-// Force to tint the drawable (useful for font with icon for example)
         ahBottomNavigation.setForceTint(true);
-
-// Display color under navigation bar (API 21+)
-// Don't forget these lines in your style-v21
-// <item name="android:windowTranslucentNavigation">true</item>
-// <item name="android:fitsSystemWindows">true</item>
         ahBottomNavigation.setTranslucentNavigationEnabled(true);
 
-// Manage titles
         ahBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
         ahBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
-        //ahBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
-        //ahBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
 
-// Use colored navigation with circle reveal effect
         ahBottomNavigation.setColored(false);
 
-// Set current item programmatically
         ahBottomNavigation.setCurrentItem(1);
 
-// Customize notification (title, background, typeface)
-        //ahBottomNavigation.setNotificationBackgroundColor(Color.parseColor("#F63D2B"));
-
-// Add or remove notification for each item
         ahBottomNavigation.setNotification("10", 1);
-// OR
-        /*AHNotification notification = new AHNotification.Builder()
-                .setText("1")
-                .setBackgroundColor(ContextCompat.getColor(DemoActivity.this, R.color.color_notification_back))
-                .setTextColor(ContextCompat.getColor(DemoActivity.this, R.color.color_notification_text))
-                .build();
-        ahBottomNavigation.setNotification(notification, 1);*/
-
-// Set listeners
         ahBottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
             public boolean onTabSelected(int position, boolean wasSelected) {
                 // Do something cool here...
                 switch (position) {
                     case 0:
-                       //item1.setColor(getResources().getColor(R.color.colorAccent));
+                        //item1.setColor(getResources().getColor(R.color.colorAccent));
                         FragmentManager fm = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
@@ -110,7 +76,7 @@ public class Principal extends AppCompatActivity {
                         FragmentManager fm2 = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction2 = fm2.beginTransaction();
 
-                        FragmentFavoritos  fragmentFavoritos = new FragmentFavoritos();
+                        FragmentFavoritos fragmentFavoritos = new FragmentFavoritos();
                         fragmentTransaction2.replace(R.id.myFragemnt, fragmentFavoritos);
                         fragmentTransaction2.commit();
 
@@ -119,7 +85,7 @@ public class Principal extends AppCompatActivity {
                         FragmentManager fm3 = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction3 = fm3.beginTransaction();
 
-                        FragmentOrden  fragmentOrden = new FragmentOrden();
+                        FragmentOrden fragmentOrden = new FragmentOrden();
                         fragmentTransaction3.replace(R.id.myFragemnt, fragmentOrden);
                         fragmentTransaction3.commit();
 
@@ -129,7 +95,7 @@ public class Principal extends AppCompatActivity {
                         FragmentManager fm4 = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction4 = fm4.beginTransaction();
 
-                        FragmentPerfil  fragmentPerfil = new FragmentPerfil();
+                        FragmentPerfil fragmentPerfil = new FragmentPerfil();
                         fragmentTransaction4.replace(R.id.myFragemnt, fragmentPerfil);
                         fragmentTransaction4.commit();
 
@@ -140,84 +106,13 @@ public class Principal extends AppCompatActivity {
                 return true;
             }
         });
-        ahBottomNavigation.setOnNavigationPositionListener(new AHBottomNavigation.OnNavigationPositionListener() {
-            @Override public void onPositionChange(int y) {
-                // Manage the new y position
-            }
-        });
-
-
-
-
-
-
-
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-
-        MainActivity mainActivity = new MainActivity();
-        fragmentTransaction.replace(R.id.myFragemnt, mainActivity);
-        fragmentTransaction.commit();
-
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.item_menu:
-
-                        FragmentManager fm = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-
-                        MainActivity mainActivity = new MainActivity();
-                        fragmentTransaction.replace(R.id.myFragemnt, mainActivity);
-                        fragmentTransaction.commit();
-
-                        return true;
-                    case R.id.item_favorito:
-
-                        FragmentManager fm2 = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction2 = fm2.beginTransaction();
-
-                        FragmentFavoritos  fragmentFavoritos = new FragmentFavoritos();
-                        fragmentTransaction2.replace(R.id.myFragemnt, fragmentFavoritos);
-                        fragmentTransaction2.commit();
-
-                        return true;
-                    case R.id.item_charola:
-                        FragmentManager fm3 = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction3 = fm3.beginTransaction();
-
-                        FragmentOrden  fragmentOrden = new FragmentOrden();
-                        fragmentTransaction3.replace(R.id.myFragemnt, fragmentOrden);
-                        fragmentTransaction3.commit();
-
-                        return true;
-                    case R.id.item_perfil:
-
-                        FragmentManager fm4 = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction4 = fm4.beginTransaction();
-
-                        FragmentPerfil  fragmentPerfil = new FragmentPerfil();
-                        fragmentTransaction4.replace(R.id.myFragemnt, fragmentPerfil);
-                        fragmentTransaction4.commit();
-
-
-                        return true;
-                }
-                return false;
-
-
-            }
-        });
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("CONSOLE","PAPA onResume");
+        Log.d("CONSOLE", "PAPA onResume");
 
 
     }
@@ -225,13 +120,13 @@ public class Principal extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("CONSOLE","PAPA onPause");
+        Log.d("CONSOLE", "PAPA onPause");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("CONSOLE","PAPA onStart");
+        Log.d("CONSOLE", "PAPA onStart");
 
     }
 }
